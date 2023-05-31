54397d2d6410ebf9f079efe010191f1c9c680c51
Leonardo Cabral <leocabrallce@gmail.com>
2023-06-01 01:16:57 +0200

    renaming store folder

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsCatalogueComponent } from './store/products-catalogue/products-catalogue.component';
-import { ProductsCatalogueItemComponent } from './store/products-catalogue-item/products-catalogue-item.component';
-import { ProductsDetailsComponent } from './store/products-details/products-details.component';
-import { ProductsCartComponent } from './store/products-cart/products-cart.component';
+import { ProductsCatalogueComponent } from './components/products-catalogue/products-catalogue.component';
+import { ProductsCatalogueItemComponent } from './components/products-catalogue-item/products-catalogue-item.component';
+import { ProductsDetailsComponent } from './components/products-details/products-details.component';
+import { ProductsCartComponent } from './components/products-cart/products-cart.component';
 import { StoreService } from '../store.service';
 
 @NgModule({
