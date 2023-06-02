72fea296c98cfbd8c99ab5db3700feeebb464115
Leonardo Cabral <leocabrallce@gmail.com>
2023-06-02 10:53:42 +0200

    return sanitized html if there's nothing to highlight

--- src/app/store/highlight.directive.ts
+++ src/app/store/highlight.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { StoreService } from '../store.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Directive({
   selector: '[highlight]'
@@ -8,13 +9,13 @@ import { StoreService } from '../store.service';
 export class HighlightDirective {
   keywords$: Observable<string>;
 
-  constructor(private storeService: StoreService, private el: ElementRef) {
+  constructor(private storeService: StoreService, private el: ElementRef, private sanitizer: DomSanitizer) {
     this.keywords$ = this.storeService.keywords$;
   }
 
   ngAfterViewChecked() {
     this.keywords$.subscribe(keywords => {
-      if (!keywords) return;
+      if (!keywords) this.sanitizer.sanitize(1, this.el.nativeElement.innerText);
       const value = this.el.nativeElement.innerText;
       const regex = new RegExp(keywords, 'gi');
       const result = value.replace(regex, "<span class='bg-yellow-300'>$&</span>");
