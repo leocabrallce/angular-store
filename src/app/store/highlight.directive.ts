5d4fbfbe347bb5d6673b123cf168027ddeacc23f
Leonardo Cabral <leocabrallce@gmail.com>
2023-06-05 22:57:45 +0200

    unsubscribing to service when destroying directive

--- src/app/store/highlight.directive.ts
+++ src/app/store/highlight.directive.ts
@@ -1,25 +1,30 @@
-import { Directive, ElementRef } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Directive, ElementRef, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { StoreService } from '../store.service';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Directive({
   selector: '[highlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnDestroy {
   keywords$: Observable<string>;
+  keywordsSubscription!: Subscription;
 
   constructor(private storeService: StoreService, private el: ElementRef, private sanitizer: DomSanitizer) {
     this.keywords$ = this.storeService.keywords$;
   }
 
   ngAfterViewChecked() {
-    this.keywords$.subscribe(keywords => {
+    this.keywordsSubscription = this.keywords$.subscribe(keywords => {
       if (!keywords) this.sanitizer.sanitize(1, this.el.nativeElement.innerText);
       const value = this.el.nativeElement.innerText;
       const regex = new RegExp(keywords, 'gi');
       const result = value.replace(regex, "<span class='bg-yellow-300'>$&</span>");
       this.el.nativeElement.innerHTML = result;
     });
+  }
+
+  ngOnDestroy() {
+    this.keywordsSubscription.unsubscribe();
   }
 }
