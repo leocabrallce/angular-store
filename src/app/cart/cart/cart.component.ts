3f1edeeedfe9bdab3b7158b6e09e3422a27f3a3c
Leonardo Cabral <leocabrallce@gmail.com>
2023-06-06 02:06:47 +0200

    enabling navitagion to and from checkout

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 import { CartService } from 'src/app/cart/cart.service';
 import Product from 'src/types/product.model';
 
@@ -10,13 +11,27 @@ import Product from 'src/types/product.model';
 export class CartComponent {
   cartItems$: Observable<Product[]>;
   total$: Observable<number>;
+  routerSubscription?: Subscription;
+  checkoutAvailable: boolean = false;
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private router: Router) {
     this.cartItems$ = this.cartService.cartItems$;
     this.total$ = this.cartService.total$;
   }
 
   removeItem(index: number): void {
     this.cartService.removeItemFromCartByIndex(index);
+  }
+
+  ngAfterViewInit(): void {
+    this.routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkoutAvailable = event.url === '/checkout';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription?.unsubscribe();
   }
 }
--- src/app/catalogue/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue/catalogue.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CatalogueService } from 'src/app/catalogue/catalogue.service';
 import Product from 'src/types/product.model';
@@ -6,6 +6,7 @@ import Product from 'src/types/product.model';
 @Component({
   selector: 'app-catalogue',
   templateUrl: './catalogue.component.html',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CatalogueComponent {
   products$: Observable<Product[]>;
