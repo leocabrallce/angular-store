e6fee0a7ee1b38cae4c1a6f125ef5d7344f14a5a
Leonardo Cabral <leocabrallce@gmail.com>
2023-06-08 20:09:27 +0200

    subscribing to route title changes

--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,19 +1,23 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { AppConfig } from 'src/types/app-config.model';
 import { APP_CONFIG } from 'src/app/app.settings';
 import { CatalogueService } from 'src/app/catalogue/catalogue.service';
 import { faStore } from '@fortawesome/free-solid-svg-icons';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
   faStore = faStore;
+  title = 'Products Store';
+  subscription: any;
 
   constructor(
     @Inject(APP_CONFIG)
     config: AppConfig,
+    route: ActivatedRoute,
     private catalogueService: CatalogueService,
   ) {
     this.language = config.language;
@@ -21,6 +25,14 @@ export class NavbarComponent {
     if (this.language !== 'en' && this.language !== 'fr') {
       this.language = 'en';
     }
+
+    this.subscription = route.title.subscribe((title) => {
+      this.title = title || this.title;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   selectLanguage(language: string) {
